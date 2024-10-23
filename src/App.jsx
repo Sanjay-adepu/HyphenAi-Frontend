4e14c32b26bbbf92111d2c6be7983760ca220921
Your Name <your_email@example.com>
2024-10-23 20:35:01 +0530

    Add other files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// client/src/App.js
+import React, { useState } from 'react';
+
+  
+
+  const App=()=>{
+
+  return (
+    <div>
+      <h1>3D Animated Video Creator</h1>
+
+    
+    </div>
+  );
+}
+
+export default App;
